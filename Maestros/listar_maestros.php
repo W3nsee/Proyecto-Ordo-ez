17deb92328c3d09b37666cd03b2c5daeecc08d9b
Ariel-al123 <132246435+Ariel-al123@users.noreply.github.com>
2024-05-14 18:48:36 -0600

    No esta terminado, se me volvio a chingar el xampp

--- Maestros/listar_maestros.php
+++ Maestros/listar_maestros.php
@@ -17,11 +17,10 @@
                $resultado = $obj->listarmaestros_maestros();
 
                while($registro = $resultado->fetch_assoc()){
-                $nombre = $registro['nombre'];
-                $apellido_paterno = $registro['apellido_paterno'];
-                $apellido_materno = $registro['apellido_materno']; 
+                $registro = $registro['nombre'] . ' ' . $registro['apellido_paterno'] . ' ' . $registro['apellido_materno'];
+              
                     echo "<div class='maestros' id='resultado'> <img class='foto' src='https://png.pngtree.com/thumb_back/fw800/background/20240104/pngtree-captivating-oceanic-majesty-reflecting-the-texture-of-light-blue-sea-water-image_13698724.png' alt='Image'>
-                    <div class='informacion_maestros'>".$nombre." ".$apellido_paterno." ".$apellido_materno."</div></div>";
+                    <div class='informacion_maestros'>" .$registro. "</div></div>";
                 }
 
         
