8df7376d419d2722c459ada8d6d7f87a3cc25808
TFoxCasillas <jcasillas7@ucol.mx>
2024-05-16 21:59:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,140 +1,102 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador</title>
-    <link rel="stylesheet" href="Style/style.css">
+    <title>Montellano</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>                    <!-- Tipografía -->
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>              <!-- Tipografía -->
     <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap" rel="stylesheet">
+<<<<<<< HEAD
     
     <!--Icons-->
     <!--Estuve Aqui TFox Parte 2-->
+=======
+>>>>>>> 9bbf95e59f7cf5318374ecd0e1d8e73afba62e10
 
+    <link rel="stylesheet" href="indexcss.css">
 </head>
-<body class="body">
+<body>
+<header>
+        <h1 class="title">Montellano</h1>
+        <nav>
+            <ul class="menu">
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#programas">Programas</a></li>
+                <li><a href="#instalaciones">Instalaciones</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    <nav>
+    <main>
+        <section id="inicio">
+            <h2>Bienvenido a Montellano</h2>
+            <img src="img/Campus.png" alt="Edificio Escolar">
+            <p>En Montellano, nos enorgullece ofrecer más de 50 años de excelencia educativa, brindando una experiencia de aprendizaje incomparable que ha marcado la vida de generaciones de estudiantes. Fundada con el compromiso de proporcionar una educación de calidad que trascienda las fronteras del aula, nos destacamos como líderes en el campo de la educación en México.</p>
+            <p>Nuestra misión es simple pero poderosa: cultivar mentes curiosas, corazones compasivos y líderes éticos que estén preparados para enfrentar los desafíos del mundo moderno con confianza y determinación. En Montellano, nos esforzamos por fomentar el desarrollo integral de cada estudiante, nutriendo sus habilidades académicas, sociales, emocionales y físicas.</p>
+            <p>¿Qué nos hace la mejor opción? Nuestro compromiso inquebrantable con la excelencia académica, combinado con un equipo dedicado de educadores apasionados y unas instalaciones de vanguardia que estimulan el aprendizaje y la creatividad. Desde nuestras aulas hasta nuestros laboratorios, desde nuestras canchas deportivas hasta nuestra biblioteca, cada rincón de Montellano está diseñado para inspirar y empoderar a nuestros estudiantes a alcanzar su máximo potencial.</p>
+            <p>En Montellano, no solo impartimos conocimientos, forjamos conexiones significativas y duraderas que trascienden el tiempo y el espacio. Nuestra comunidad escolar es una familia unida, donde se fomenta el compañerismo, el respeto mutuo y la colaboración. Aquí, cada estudiante es valorado, cada voz es escuchada y cada logro es celebrado.</p>
+            <p>Si buscas una educación que marque la diferencia, Montellano es tu destino. Únete a nosotros y descubre por qué somos reconocidos como la mejor opción educativa en México. Juntos, construiremos un futuro brillante y lleno de posibilidades para cada uno de nuestros estudiantes.</p>
+        </section>
+        <section id="programas">
+            <h2>Programas Académicos</h2>
+            <img src="img/Aula.png" alt="Aula">
+            <p>Bienvenido al programa académico de la Escuela Montellano, una institución educativa dedicada a brindar una educación de excelencia a nuestros estudiantes. Nuestro programa se enfoca en proporcionar una sólida base académica, fomentar el desarrollo personal y preparar a nuestros alumnos para enfrentar los desafíos del mundo moderno.</p>
+            <h3>Áreas de Estudio</h3>
+            <p>Nuestra oferta académica abarca una amplia gama de áreas de estudio, incluyendo:</p>
+            <ul>
+                <li>Matemáticas VI</li>
+                <li>Inglés</li>
+                <li>Programación de Internet II</li>
+                <li>Programación IV</li>
+                <li>Analisis y Diseño de Sistemas</li>
+                <li>Física IV</li>
+                <li>Metodlogía de la Investigación</li>
+            </ul>
+            <p>Además, ofrecemos programas de enriquecimiento y actividades extracurriculares que permiten a nuestros estudiantes explorar sus intereses y desarrollar habilidades complementarias.</p>
+            <h3>Metodologias de Enseñanza</h3>
+            <p>En la Escuela Montellano, utilizamos una metodología de enseñanza basada en el aprendizaje activo y centrado en el estudiante. Nuestros docentes emplean estrategias didácticas innovadoras, como:</p>
+            <ul>
+            <li>Aprendizaje basado en proyectos</li>
+            <li>Aprendizaje colaborativo</li>
+            <li>Uso de tecnologías educativas</li>
+            <li>Enfoque en el desarrollo de habilidades blandas</li>
+            </ul>
+            <p>Esto permite a nuestros estudiantes adquirir conocimientos sólidos, desarrollar habilidades prácticas y fomentar su pensamiento crítico y creativo.</p>
 
-        <div class="Menu">
+        </section>
 
-            <div class="btn-desplegar">
+        <section id="instalaciones">
+            <h2>Instalaciones y Recursos</h2>
+            <p>Para brindar una experiencia educativa integral, la Escuela Montellano cuenta con los siguientes servicios y recursos:</p>
+            <h3>Biblioteca y centro de recursos de aprendizaje</h3>
+            <img src="img/Biblioteca.png" alt="Biblioteca" width = "850px", height="500px">
+            <h3>Laboratorios de ciencias y tecnología</h3>
+            <img src="img/Laboratorio.jpg" alt="Laboratorio" width = "800px", height="532px">
+            <img src="img/Modulo.png" alt="Modulo" width = "800px", height="532px">
+            <h3>Instalaciones deportivas y áreas de recreación</h3>
+            <img src="img/Basquetbol.png" alt="Cancha" width = "800px", height="532px">
+            <p>Nuestro objetivo es crear un entorno de aprendizaje enriquecedor y estimulante que permita a nuestros estudiantes alcanzar su máximo potencial.</p>
+            <p>Si deseas más información sobre nuestro programa académico, te invitamos a visitar nuestra página web o comunicarte con nuestro equipo de admisiones. Estaremos encantados de atender tus consultas y guiarte en el proceso de inscripción.</p>
+            <p>¡Bienvenido a la Escuela Montellano, donde la excelencia académica y el desarrollo integral de nuestros estudiantes son nuestra prioridad!</p>
+        </section>
 
-                <img class="img-menu" src="img/menu.png" width="50px" height="50px">
-
-            </div>
-
-            <a href="Alta_Admin/Alta_Usuario.php">
-
-                <div class="opccionesMenu">
-
-                    <li class="text-opcciones">Alta de usuario</li>
-
-                </div>
-
-            </a>
-
-            <a href="Alta_Admin/Alta_Asignatura.php">
-
-                <div class="opccionesMenu">
-
-                    <li class="text-opcciones">Alta de asignatura</li>
-
-                </div>
-
-            </a>
-
-            <a href="Matricular_Admin/Matricualar_Alumno.php">
-
-                <div class="opccionesMenu">
-
-                    <li class="text-opcciones">Matricular alumno en asignatura</li>
-
-                </div>
-
-            </a>
-
-            <a href="Baja_Admin/Baja_Usuario.php">
-
-                <div class="opccionesMenu">
-
-                    <li class="text-opcciones">Baja de usuario</li>
-
-                </div>
-
-            </a>
-
-            <a href="Baja_Admin/Baja_Asignatura.php">
-
-                <div class="opccionesMenu">
-
-                    <li class="text-opcciones">Baja de asignatura</li>
-
-                </div>
-
-            </a>
-
-            <a href="Modificacion_Admin/Modificar_Usuario.php">
-
-                <div class="opccionesMenu">
-
-                    <li class="text-opcciones">Modificar datos de usuario</li>
-
-                </div>
-
-            </a>
-
-            <a href="Modificacion_Admin/Modificar_Asignatura.php">
-
-                <div class="opccionesMenu">
-
-                    <li class="text-opcciones">Modificar datos de asignatura</li>
-
-                </div>
-
-            </a>
-
-            <a class="btn-regresar" href="index.php">
-
-                <div >
-
-                    <li class="text-regresar">Regresar</li>
-
-                </div>
-
-            </a>
-
-        </div>
-
-    </nav>
-
-    <section>
-
-        <div class="titulo">
-
-            <h1 class="text-titulo">ADMINISTRADOR</h1>
-
-        </div>
-
-        <div class="logo_fondo">
-
-            <img src="img/3.png" width="600px">
-
-        </div>
-
-    </section>
+        <section id="contacto">
+            <h2>Contacto</h2>
+            <ul>
+                <li>Dirección: Calle Principal #123, Montellano, Colima, México</li>
+                <li>Teléfono: +312 456 7890</li>
+                <li>Correo Electrónico: info@escuelamontellano.edu</li>
+                <li>Horario de Atención: Lunes a Viernes de 8:00 AM a 4:00 PM</li>
+            </ul>
+        </section>
+    </main>
 
     <footer>
-
-        <div class="footer">
-
-            <h3 class="text-footer">Todos los derechos reservados &copy;</h3>
-
-        </div>
-
+        <p>&copy; <?php echo date("Y"); ?> Montellano</p>
     </footer>
-    
 </body>
 </html>
+
