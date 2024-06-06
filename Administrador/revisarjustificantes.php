d729e341ca6a401de01c7a51925d0159e891032b
M-Arteaga <Marteaga3@ucol.mx>
2024-06-06 10:27:20 -0600

    terminen la ventana de aceptar o rechazar justificante por favor

---
+++ Administrador/revisarjustificantes.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+$alumno = isset($_SESSION['id']) ? $_SESSION['id'] : ''; // Obtener id del maestro
+
+require_once("contacto.php");
+$obj = new contacto();
+$resultado = $obj->listarjustificantes();
+
+if($resultado->num_rows > 0) {
+        
+    echo "<h1>Aprobar o Rechazar Justificante</h1>";
+    echo "<form action='' method='post'>";
+    echo "<select name='justificante'>";
+    
+    while ($registro = $resultado->fetch_assoc()) {
+        echo "<option value='" . $registro["id_alumno"] . "'>" . $registro["fecha_falta"] . " - " . $registro["motivo"] . "</option>";
+    }
+    
+    echo "</select>";
+    echo "<br><br>";
+    echo "<input type='submit' name='seleccionar' value='Seleccionar'>";
+    echo "</form>";
+    
+} else {
+    echo "No has solicitado ningún justificante.";
+}
+
+if(isset($_POST["seleccionar"])) {
+    $idjustificante = $_POST["justificante"];
+    echo "<h2>Selecciona una acción:</h2>";
+    echo "<form action='' method='post'>";
+    echo "<input type='radio' name='accion' value='aceptar'>Aceptar ";
+    echo "<input type='radio' name='accion' value='rechazar'>Rechazar ";
+    echo "<br><br>";
+    echo "<input type='hidden' name='idjustificante' value='$idjustificante'>";
+    echo "<input type='submit' name='enviar' value='Enviar'>";
+    echo "</form>";
+}
+?>
