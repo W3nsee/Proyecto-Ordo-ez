a5dc5ea9eb1eb8ae9d41947a78841c5681d49d76
W3nsee <guzzmx7@gmail.com>
2024-06-06 06:13:04 -0600

    Alta Alumno

--- Administrador/altaalumno.php
+++ Administrador/altaalumno.php
@@ -1,52 +1,128 @@
 <!DOCTYPE html>
 <html>
    <head>
+
        <title>Alta Alumnos</title>
        <meta charset="utf-8">
        <meta name="description" content="Dar de alta un alumno"/>
+       <link rel="stylesheet" href="Administrador/CSS/alta_alumno.css">
+
+       <!--Fuentes de google-->
+
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap" rel="stylesheet">
+
    </head>
 
    <body>
-      <h1>Dar de Alta un Alumno</h1> 
 
-   <form action="" method="post">
-   Nombres: <input type="text" name="nombre" placeholder="Ejem. José María" required><br/><br/>
-   Apellido paterno: <input type="text" name="apellidopaterno" placeholder="Ejem. Pérez" required><br/><br/>
-   Apellido materno: <input type="text" name="apellidomaterno" placeholder="Ejem. Gómez" required><br/><br/>
-   Fecha de nacimiento: <input type="date" name="fechanacimiento" max="<?php echo date('Y-m-d'); ?>" required><br/><br/> <!-- Añadir el atributo max -->
-   Telefono: <input type="text" name="telefono" placeholder="Ejem. 312 666 6666" required><br/><br/>
-   Correo: <input type="email" name="correo" placeholder="Ejem. nombre6@gmail.com" required><br/><br/>
-   Sexo:
-      <label>
-         <input type="radio" name="sexo" value="F" checked>Femenino
-      </label>
-      <label>
-         <input type="radio" name="sexo" value="M">Masculino
-      </label><br/><br/>
-   Grado:
-      <label>
-         <input type="radio" name="grado" value="1" checked>1
-      </label>
-      <label>
-         <input type="radio" name="grado" value="2">2
-      </label>
-      <label>
-         <input type="radio" name="grado" value="3">3
-      </label><br/><br/>
-   Grupo:
-      <label>
-         <input type="radio" name="grupo" value="A" checked>A
-      </label>
-      <label>
-         <input type="radio" name="grupo" value="B">B
-      </label>
-      <label>
-         <input type="radio" name="grupo" value="C">C
-      </label><br/><br/>
+      <h1 class="titulo">Dar de Alta un Alumno</h1> 
 
-      Contraseña: <input type="text" id="contrasena" name="contrasena" readonly><br/><br/>   
-      <input type="button" value="Generar Contraseña" onclick="generarContrasena()">
-   <input type="submit" name="insertar" value="Guardar">
+<form action="" method="post">
+
+   <div class="cont_form">
+
+      <div class="cont_nombres">
+
+         <h2 class="label_nombres">Nombres</h2>
+         <input class="input_nombres" type="text" name="nombre" placeholder="Ejem. José María" required> <br/><br/>
+
+      </div>
+      
+      <div class="cont_apellido_paterno">
+
+         <h2 class="label_apellido_paterno">Apellido paterno</h2>
+         <input class="input_apellido_paterno" type="text" name="apellidopaterno" placeholder="Ejem. Pérez" required><br/><br/>
+
+      </div>
+
+      <div class="cont_apellido_materno">
+
+         <h2 class="label_apellido_materno">Apellido materno</h2>
+         <input class="input_apellido_materno" type="text" name="apellidomaterno" placeholder="Ejem. Gómez" required><br/><br/>
+
+      </div>
+
+      <div class="cont_fecha_nacimiento">
+      
+         <h2 class="label_fecha_nacimiento">Fecha de nacimiento</h2>
+
+         <input class="input_fecha_nacimiento" type="date" name="fechanacimiento" max="<?php echo date('Y-m-d'); ?>" required><br/><br/><!-- Añadir el atributo max -->
+
+      </div>
+
+      <div class="cont_telefono">
+
+         <h2 class="label_telefono">Telefono</h2>
+         <input class="input_telefono" type="text" name="telefono" placeholder="Ejem. 312 666 6666" required><br/><br/>
+
+      </div>
+
+      <div class="cont_correo">
+
+         <h2 class="label_correo">Correo</h2>
+         <input class="input_correo" type="email" name="correo" placeholder="Ejem. nombre6@gmail.com" required><br/><br/>
+
+      </div>
+
+      <div class="cont_sexo">
+
+         <h2 class="label_sexo">Sexo</h2>
+            
+         <div class="border_sexo">
+
+            <input class="input_sexo_femenino" type="radio" name="sexo" value="F" checked>
+            <input class="input_sexo_masculino" type="radio" name="sexo" value="M"> <br/><br/>
+
+         </div>
+
+      </div>
+
+      <div class="cont_grado">
+
+         <h2 class="label_grado">Grado</h2>
+
+         <div class="border_grado">
+
+            <input class="input_grado_1" type="radio" name="grado" value="1" checked> 
+            <input class="input_grado_2" type="radio" name="grado" value="2"> 
+            <input class="input_grado_3" type="radio" name="grado" value="3"> <br/><br/>
+
+         </div>
+         
+      </div>
+      
+      <div class="cont_grupo">
+
+         <h2 class="label_grupo">Grupo</h2>
+
+         <div class="border_grupo">
+
+            <input class="input_grupo_a" type="radio" name="grupo" value="A" checked> 
+            <input class="input_grupo_b" type="radio" name="grupo" value="B">
+            <input class="input_grupo_c" type="radio" name="grupo" value="C"><br/><br/>
+
+         </div>
+
+      </div>
+
+      <div class="cont_password">
+
+         <h2 class="label_password">Contraseña</h2>
+         <input class="input_password" type="password" id="contrasena" name="contrasena" readonly><br/><br/>
+         <input class="boton_generar" type="button" value="Generar Contraseña" onclick="generarContrasena()">
+
+      </div>
+
+      <div class="button_guardar">
+
+         <input class="boton_guardar" type="submit" name="insertar" value="Guardar">
+
+      </div>
+
+   </div>
+
 </form>
 
 <?php
